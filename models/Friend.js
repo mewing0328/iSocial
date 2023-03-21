d1a7b9659544242fdec4fbb0e4afc374e24a082a
MEwing <masandraewing@gmail.com>
2023-03-21 15:50:16 -0400

    Friend Schema

--- models/Friend.js
+++ models/Friend.js
@@ -1,4 +1,18 @@
 const { Schema, Types } = require('mongoose');
 
+const friendSchema = new Schema(
+    {
+        friendId: {
+            type: Schema.Types.ObjectId(),
+            default: () => new Types.ObjectId(),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
 
 module.exports = friendSchema;
